2270fee96cf73208a230280794839b5f6c74351f
sagsoz06 <designer@visualturk.net>
2019-03-15 01:23:42 +0300

    Updating theme

--- views/partials/metadata.blade.php
+++ views/partials/metadata.blade.php
@@ -1,3 +1,7 @@
 {!! seo_helper()->render() !!}
 {!! Theme::style("plugins/revslider/public/assets/css/settings.css") !!}
+@if(App::environment()=='local')
 {!! Theme::style("css/style-core.css") !!}
+@else
+<link rel="stylesheet" href="{{ elixir('css/style-core.min.css', 'themes/rubber') }}" />
+@endif
